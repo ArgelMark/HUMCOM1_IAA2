04626c31d10468dc82a0e8f2bf0b195654a391fb
ArgelMark <98304005+ArgelMark@users.noreply.github.com>
2022-05-12 10:36:06 +0800

    Add files via upload

---
+++ Script15.js
@@ -0,0 +1,20 @@
+let firstname ="Marky";
+let age= 19;
+let pnum= 09123678915;
+let mail= "20215892@s.ubaguio.edu";
+let mailadd= "Baguio City, Philippines";
+let prog= "Information Technology";
+let IDnum= 20215892;
+let ylev= "First Year";
+let sec= "IAA2";
+
+
+document.getElementById("firstname").value = firstname;
+document.getElementById("myAge").value = age;
+document.getElementById("pnum").value = pnum;
+document.getElementById("mail").value = mail;
+document.getElementById("mailadd").value = mailadd;
+document.getElementById("prog").value = prog;
+document.getElementById("IDnum").value = IDnum;
+document.getElementById("ylev").value = ylev;
+document.getElementById("sec").value = sec;
